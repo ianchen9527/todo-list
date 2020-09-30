b16e1369f0a228b34c75105d473d9370f4b03780
Ian Chen <ianchen9527@gmail.com>
2020-10-01 01:54:17 +0800

    Implement the homepage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./index.module.scss";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className={styles["wrapper"]}>
+      <div className={styles["title"]}>
+        Welcome to the <b>Proton Note Demo</b>
+      </div>
+      <Link to="/notes" className={styles["enter-button"]}>
+        <div className={styles["logo"]} />
+        <div className={styles["description"]}>Enter</div>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
