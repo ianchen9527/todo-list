91e3d99aeeb87d9866935cd6fd92e8d8d0f958a0
Ian Chen <ianchen9527@gmail.com>
2020-10-01 23:55:17 +0800

    Create tests of selectors/responses/notes

---
+++ src/selectors/responses/notes/index.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { getNotes } from "./index";
+
+describe("notes", () => {
+  let mockStore;
+
+  describe("getNotes", () => {
+    let expectedNotes;
+
+    beforeEach(() => {
+      expectedNotes = "expectedNotes";
+      mockStore = { responses: { notes: expectedNotes } };
+    });
+
+    it("should return state.responses.notes", () => {
+      expect(getNotes(mockStore)).toBe(expectedNotes);
+    });
+  });
+});
