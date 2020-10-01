57463b0742f67fafa0a0ca381d720df3232311e3
Ian Chen <ianchen9527@gmail.com>
2020-10-02 00:49:17 +0800

    Create tests of sagas/noteList

--- src/sagas/noteList/index.js
+++ src/sagas/noteList/index.js
@@ -2,18 +2,17 @@ import { call, put, takeEvery } from "redux-saga/effects";
 import apis from "../../apis";
 import { updateNotes } from "../../reducers/responses/notes";
 
-const ON_DID_MOUNT = `${__filename}/ON_DID_MOUNT`;
-const onDidMount = { type: ON_DID_MOUNT };
+export const ON_DID_MOUNT = `${__filename}/ON_DID_MOUNT`;
+
+export const onDidMount = { type: ON_DID_MOUNT };
 
 export const getOnDidMount = () => ({ dispatch }) => dispatch(onDidMount);
 
-function* sagaOnDidMount() {
+export function* sagaOnDidMount() {
   const notes = yield call(apis.fetchNotes);
   yield put(updateNotes(notes));
 }
 
-function* watcher() {
+export default function* watcher() {
   yield takeEvery(ON_DID_MOUNT, sagaOnDidMount);
 }
-
-export default watcher;
