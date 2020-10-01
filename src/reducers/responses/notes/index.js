a1f2a92b3efeaccb865c234afa63096137cf108d
Ian Chen <ianchen9527@gmail.com>
2020-10-02 01:06:52 +0800

    Create tests of reducers/responses/notes

--- src/reducers/responses/notes/index.js
+++ src/reducers/responses/notes/index.js
@@ -2,7 +2,9 @@ export const UPDATE_NOTES = `${__filename}/UPDATE_NOTES`;
 
 export const updateNotes = (notes) => ({ type: UPDATE_NOTES, payload: notes });
 
-export default (state = [], action) => {
+const initialState = [];
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NOTES:
       return action.payload;
