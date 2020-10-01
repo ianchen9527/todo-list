e9282262e20ba7ae5394423723c93b9bd2391842
Ian Chen <ianchen9527@gmail.com>
2020-10-01 23:10:25 +0800

    Add Notes to the router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import routes from './constants/routes';
-import Home from './components/Home';
+import Home from './components/pages/Home';
+import Notes from './components/pages/Notes';
 
 function App() {
   return (
     <Switch>
       <Route path={routes.HOME} exact component={Home} />
+      <Route path={routes.NOTES} exact component={Notes} />
     </Switch>
   );
 }
