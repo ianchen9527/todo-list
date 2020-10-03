6a513d22db8b0f714a8eda67e5de8e6605239eb1
Ian Chen <ianchen9527@gmail.com>
2020-10-04 00:58:50 +0800

    Update components/pages/Notes

--- src/components/pages/Notes/index.jsx
+++ src/components/pages/Notes/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./index.module.scss";
 import NoteList from "../../../containers/NoteList";
+import NoteIncreaser from "../../../containers/NoteIncreaser";
 
 const Notes = () => {
   return (
     <div className={styles["wrapper"]}>
-      <div className={styles["increaser-wrapper"]} />
+      <div className={styles["note-increaser-wrapper"]}>
+        <NoteIncreaser />
+      </div>
       <div className={styles["note-list-wrapper"]}>
         <NoteList />
       </div>
-      <div className={styles["note-viewer-wrapper"]} />
+      <div className={styles["note-viewer-wrapper"]}>
+        Please select a note to view or edit...
+      </div>
     </div>
   );
 };
