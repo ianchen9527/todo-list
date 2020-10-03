e4f8c12f456515dc048bb9dd20e87c8f0ca29fde
Ian Chen <ianchen9527@gmail.com>
2020-10-04 00:59:17 +0800

    Create components/elements/Button

---
+++ src/components/elements/Button/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./index.module.scss";
+import PropTypes from "prop-types";
+import cx from "classnames";
+
+const getWrapperClassName = (disabled) =>
+  cx({
+    [styles["wrapper"]]: true,
+    [styles["disabled"]]: disabled,
+  });
+
+const Button = ({ icon, loading, onClick, text, disabled }) => {
+  return (
+    <div className={getWrapperClassName(disabled)} onClick={onClick}>
+      {icon && <div className={styles[`icon-${icon}`]} />}
+      {text}
+      {loading && <div className={styles["loading"]} />}
+    </div>
+  );
+};
+
+Button.oropTypes = {
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool,
+  icon: PropTypes.string,
+  text: PropTypes.string,
+};
+
+Button.defaultProps = {
+  onClick: null,
+  diabled: false,
+  loading: false,
+  icon: "add",
+  text: "",
+};
+
+export default Button;
