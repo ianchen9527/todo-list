d6e8a606158d4ad71f25ba5c944e1ca742919ee1
Ian Chen <ianchen9527@gmail.com>
2020-10-04 12:54:27 +0800

    Add new mock API

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,11 @@
 import fetchNotes from "./fetchNotes";
+import saveNotes from "./saveNotes";
+import deleteNote from "./deleteNote";
 import addNote from "./addNote";
 
 export default {
   fetchNotes,
   addNote,
+  saveNotes,
+  deleteNote,
 };
