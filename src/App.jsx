d53053ba1e7cf7ac0d956adb87ff4f483083b603
Ian Chen <ianchen9527@gmail.com>
2020-10-04 12:54:08 +0800

    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import routes from './constants/routes';
 import Home from './components/pages/Home';
 import Notes from './components/pages/Notes';
 import Note from './components/pages/Note';
+import NoteEdit from './components/pages/NoteEdit';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path={routes.HOME()} exact component={Home} />
       <Route path={routes.NOTES()} exact component={Notes} />
       <Route path={routes.NOTE()} exact component={Note} />
+      <Route path={routes.NOTE_EDIT()} exact component={NoteEdit} />
     </Switch>
   );
 }
